1fb4bcc67a24d60da43481d8aad3613f559ed5ef
Samuel Richardson <git@richardsonsamuel.com>
2019-09-23 00:09:54 +1000

    build

---
+++ docs/js/chunk-2d217a6b.8303601c.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d217a6b"],{c844:function(n,t,i){"use strict";i.r(t);var e=function(){var n=this,t=n.$createElement,i=n._self._c||t;return i("div",{staticClass:"animated fadeIn"},[n._v("\n  Page 3\n")])},a=[],l={name:"project",data:function(){return{slide:0,sliding:null}},methods:{onSlideStart:function(n){this.sliding=!0},onSlideEnd:function(n){this.sliding=!1}}},s=l,d=i("2877"),c=Object(d["a"])(s,e,a,!1,null,null,null);t["default"]=c.exports}}]);
+//# sourceMappingURL=chunk-2d217a6b.8303601c.js.map
