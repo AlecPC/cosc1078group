88e18eeec4cff402573bdda7072289eda495dca4
Samuel Richardson <git@richardsonsamuel.com>
2019-09-23 00:16:00 +1000

    whoops

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   lintOnSave: false,
   runtimeCompiler: true,
-  publicPath: '/cosc1078/',
+  publicPath: '/cosc1078group/',
   outputDir: path.resolve(__dirname, 'docs'),
   // assetsDir: '/'
 }
