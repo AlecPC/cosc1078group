ce7a18d4a12e455c642bfee7f998191e88503749
Samuel Richardson <git@richardsonsamuel.com>
2019-09-23 00:14:56 +1000

    fix

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   lintOnSave: false,
   runtimeCompiler: true,
-  publicPath: '/',
+  publicPath: '/cosc1078/',
   outputDir: path.resolve(__dirname, 'docs'),
   // assetsDir: '/'
 }
