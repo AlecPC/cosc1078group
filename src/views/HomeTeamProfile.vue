328f175a366587cd84f229c469117c82015a98bf
Samuel Richardson <git@richardsonsamuel.com>
2019-09-22 21:52:40 +0800

    commit

--- src/views/HomeTeamProfile.vue
+++ src/views/HomeTeamProfile.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="animated">
-    <div class="row py-5 px-3 bg-light">
+    <div class="row px-5 py-5 bg-light">
       <div class="col">
         <h1>
           Anthony 
@@ -31,6 +31,60 @@
           information by the way it sounds, and you have an easier time understanding spoken instructions than written ones. You often learn by
           reading out loud because you have to hear it or speak it in order to know it.
         </p>
+
+        <h2>
+          The hobby test result
+        </h2>
+        <p>
+          You got: Computer programming
+          Programming can be an intimidating hobby, but if you want to challenge yourself and also acquire a useful skill, it’s a great thing
+          to do! There’s plenty of spaces online where you can teach yourself for free, as well as paid courses you can attend depending
+          on how much you want to spend.
+
+        </p>
+      </div>
+    </div>
+    <div class="row px-5 py-5 bg-dark">
+      <div class="col">
+        <h1>Samuel</h1>
+        <h3 style="font-weight: 100">I'm a student, developer, beach lifeguard.</h3>
+        <br />
+        <p class="lead">
+          Originally from Sydney, moved to Melbourne in 2007
+          <i
+            title="au"
+            id="au"
+            class="flag-icon flag-icon-au"
+          ></i> I've always had a passion for developing things 👨🏼‍💻 in particular software. My IT interested started originally from a young age with 🛠 tweaking with Windows Servers etc. and eventually growing from there.
+        </p>
+        <p
+          class="lead"
+        >I now work as a 📲 Software Developer for Life Saving Victoria and 💼 consult independently for a few different companies with their 📈 Digital Stategies and bespoke software products. Whilst alot of my 💡 knowledge in this area was self taught, it is through my employment at Life Saving Victoria where I have developed extensive knowledge in building 📟 REST based APIs, architecting ☁️ serveless infrastructure on AWS, designing 👾 automated build and deploy (CI/CD) pipelines for the software, and the 💻 front end interfaces to integrate with it.</p>
+        <p class="lead">
+          As a hobby I also am a 🏖 beach/jetski lifeguard and now Lifeguard Communications Supervisor 🆘 which involves managing incidents from all the beaches accross the state. In my down time I also take my 🔐 information security and 👤 privacy so seriously – I have a 16RU server rack at my place of which contains my Unifi Security Gateway, Unifi Switches, and 2 Dell PowerEdge Servers (1 for ESXi and 1 for Storage). All in an effort to route my traffic via
+          <i
+            title="ch"
+            id="ch"
+            class="flag-icon flag-icon-ch"
+          ></i> Switzerland and secure 🔑 my personal data.
+        </p>
+        <h2
+          style="font-weight: 100"
+        >Below are the Myers-Briggs test, learning style test, and emotional intelligence test results. These tests are important to show how my personality would fit within a workplace environment both in regards to my own efficency and the workplace culture.</h2>
+        <br />
+        <div class="card-deck">
+          <div class="card">
+            <img src="/img/test1.png" class="card-img-top" alt="..." />
+          </div>
+          <div class="card">
+            <img src="/img/test2.png" class="card-img-top" alt="..." />
+          </div>
+          <div class="card">
+            <img src="/img/test3.png" class="card-img-top" alt="..." />
+          </div>
+        </div>
+        <br>
+        <p class="lead col-md-7 px-0">The above results show I'm a very logical thinker and have a measured approach to problems whilst being very confident about my opinions. They also show I'm aware of my actions and how they effect others. In a team evnrionemnt its important I consider the views and thoughts of others and try to not to suppress ideas by overly advocating my own.</p>
       </div>
     </div>
   </div>
